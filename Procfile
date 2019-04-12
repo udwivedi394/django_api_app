b6b893aa2953e6d92746a64886a4e0047cfb3a89
Utkarsh Dwivedi <utkarshdwivedi394@gmail.com>
2019-04-12 22:18:07 +0530

    change settings
    Signed-off-by: Utkarsh Dwivedi <utkarshdwivedi394@gmail.com>

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn fyle_task.wsgi --log-file fyle_error.log --preload
+web: gunicorn fyle_task.wsgi --log-file fyle_error.log --preload;
 
--- fyle_task/settings.py
+++ fyle_task/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0rp)r!y(=ws67%_5!yd_b9-a=ulqxg-n)v9_)sl#wov-jby$n^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cryptic-ravine-52771.herokuapp.com']
+ALLOWED_HOSTS = ['cryptic-ravine-52771.herokuapp.com', '0.0.0.0']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'fyle_task.urls'
@@ -119,6 +120,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
